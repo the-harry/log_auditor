b7ddb7b87af40b3285dbf04d3d11070c73a457f2
Harry <matheusthebr@gmail.com>
2021-12-03 04:22:45 -0300

    Created main parser class.

--- lib/log_auditor.rb
+++ lib/log_auditor.rb
@@ -1,7 +1,23 @@
 # frozen_string_literal: true
 
 require_relative 'log_auditor/version'
+require_relative 'log_auditor/errors'
+require_relative 'log_auditor/reader'
+require_relative 'log_auditor/formatter'
+require_relative 'log_auditor/presenter'
 
-# Your code goes here...
-class LogAuditor::Error < StandardError
+class LogAuditor::Parser
+  attr_reader :reader, :formatter, :presenter
+
+  def initialize(reader:, formatter:, presenter:)
+    @reader = reader
+    @formatter = formatter
+    @presenter = presenter
+  end
+
+  def parse!
+    raw_data = reader.read!
+    formatted_data = formatter.new(raw_data).format!
+    presenter.new(formatted_data).present!
+  end
 end
